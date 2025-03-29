87897da6a83b4700cf713f496411fc8027dd5a17
Samuel Nyarko <samuelnyarko003@gmail.com>
2025-03-29 19:48:02 +0000

    Six complete

--- practice_six.dart
+++ practice_six.dart
@@ -42,31 +42,115 @@ void main() {
 }
 
 Write a dart program to create an enum class for gender [male, female, others] and print all values.
-Write a dart program to create a class Animal with properties [id, name, color]. Create another class called Cat and extends it from Animal. Add new properties sound in String. Create an object of a Cat and print all details.
-Write a dart program to create a class Camera with private properties [id, brand, color, price]. Create a getter and setter to get and set values. Also, create 3 objects of it and print all details.
-Create an interface called Bottle and add a method to it called open(). Create a class called CokeBottle and implement the Bottle and print the message “Coke bottle is opened”. Add a factory constructor to Bottle and return the object of CokeBottle. Instantiate CokeBottle using the factory constructor and call the open() on the object.
-Create a simple quiz application using oop that allows users to play and view their score.
 
- */
+class Person {
+  String? id, name;
+  Genders? gender;
 
-class House {
-  String? id, name, price;
-
-  House(this.id, this.name, this.price);
+  Person(this.id, this.name, this.gender);
 
   printDetails() {
     print(id);
     print(name);
-    print(price);
+    print(gender);
   }
 }
 
+enum Genders { male, female, others }
 
-enum Genders{
-  male,
-  female,
-  others
+void main() {
+  Person person = Person('1', 'Sam', Genders.male);
+
+  person.printDetails();
+}
+
+Write a dart program to create a class Animal with properties [id, name, color]. Create another class called Cat and extends it from Animal. Add new properties sound in String. Create an object of a Cat and print all details.
+
+class Animal {
+  String? id, name, color;
+
+  Animal(this.id, this.name, this.color);
+}
+
+class Cat extends Animal {
+  String sound;
+  Cat(String id, String name, String color, this.sound)
+      : super(id, name, color);
+
+  printDetails() {
+    print(id);
+    print(name);
+    print(color);
+    print(sound);
+  }
 }
 
 void main() {
+  Cat cat = Cat('1', 'Cat', 'White', 'Meow');
+  cat.printDetails();
+}
+
+Write a dart program to create a class Camera with private properties [id, brand, color, price]. Create a getter and setter to get and set values. Also, create 3 objects of it and print all details.
+
+class Camera {
+  String? _id, _brand, _color, _price;
+
+  Camera();
+
+  set setId(String id) => _id = id;
+  set setBrand(String brand) => _brand = brand;
+  set setColor(String color) => _color = color;
+  set setPrice(String price) => _price = price;
+
+  String get id => this._id!;
+  String get brand => this._brand!;
+  String get color => this._color!;
+  String get price => this._price!;
+}
+
+void main() {
+  Camera camera = Camera();
+
+  camera.setId = '1';
+  camera.setBrand = 'Canon';
+  camera.setColor = 'Blue';
+  camera.setPrice = '20';
+
+  print(camera.id);
+  print(camera.brand);
+  print(camera.color);
+  print(camera.price);
+}
+
+Create an interface called Bottle and add a method to it called open(). Create a class called CokeBottle and implement the Bottle and print the message “Coke bottle is opened”. Add a factory constructor to Bottle and return the object of CokeBottle. Instantiate CokeBottle using the factory constructor and call the open() on the object.
+
+
+
+Create a simple quiz application using oop that allows users to play and view their score.
+
+ */
+
+class Bottle {
+  void open() {
+    //print(object)
+  }
+
+  factory Bottle.namedConstructor() {
+    CokeBottle cokeBottle = CokeBottle();
+    return cokeBottle;
+  }
+}
+
+class CokeBottle implements Bottle {
+  @override
+  void open() {
+    // TODO: implement open
+    print('Coke bottle is opened');
+  }
+}
+
+void main() {
+  Bottle bottle = Bottle.namedConstructor();
+  bottle.open();
+
 }
